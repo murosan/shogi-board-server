3660b8ca4121dc121344a990fd5e0ebe1e47742a
murosan <m.murota.dev@gmail.com>
2018-10-29 00:37:21 +0900

    test for options

--- app/domain/entity/engine/option/option_test.go
+++ app/domain/entity/engine/option/option_test.go
@@ -74,35 +74,125 @@ func TestCheck_Usi(t *testing.T) {
 }
 
 func TestFileName_GetName(t *testing.T) {
+	cases := []struct {
+		in   FileName
+		want []byte
+	}{
+		{FileName{[]byte("file-name"), []byte("engine.exe"), []byte("engine.exe")}, []byte("file-name")},
+		{FileName{Name: []byte("")}, []byte("")},
+		{FileName{[]byte(" "), []byte("engine.exe"), []byte("engine.exe")}, []byte(" ")},
+		{FileName{Name: []byte("%\n|t\t")}, []byte("%\n|t\t")},
+	}
 
+	for i, c := range cases {
+		getNameTestHelper(t, i, c.in, c.want)
+	}
 }
 
 func TestFileName_Usi(t *testing.T) {
+	cases := []struct {
+		in   FileName
+		want []byte
+	}{
+		{FileName{[]byte("file-name"), []byte("engine.exe"), []byte("engine.exe")}, []byte("setoption name file-name value engine.exe")},
+		{FileName{Name: []byte("")}, []byte("setoption name  value ")},
+		{FileName{[]byte(" "), []byte("engine.exe"), []byte("engine.exe")}, []byte("setoption name   value engine.exe")},
+		{FileName{Name: []byte("%\n|t\t")}, []byte("setoption name %\n|t\t value ")},
+	}
 
+	for i, c := range cases {
+		usiTestHelper(t, i, c.in, c.want)
+	}
 }
 
 func TestSelect_GetName(t *testing.T) {
+	cases := []struct {
+		in   Select
+		want []byte
+	}{
+		{Select{[]byte("sel-name"), 1, [][]byte{[]byte("one"), []byte("two"), []byte("three")}}, []byte("sel-name")},
+		{Select{Name: []byte("")}, []byte("")},
+		{Select{[]byte(" "), 2, [][]byte{[]byte("one"), []byte("two"), []byte("three")}}, []byte(" ")},
+		{Select{Name: []byte("%\n|t\t")}, []byte("%\n|t\t")},
+	}
 
+	for i, c := range cases {
+		getNameTestHelper(t, i, c.in, c.want)
+	}
 }
 
 func TestSelect_Usi(t *testing.T) {
+	cases := []struct {
+		in   Select
+		want []byte
+	}{
+		{Select{[]byte("sel-name"), 1, [][]byte{[]byte("one"), []byte("two"), []byte("three")}}, []byte("setoption name sel-name value two")},
+		{Select{[]byte(" "), 2, [][]byte{[]byte("one"), []byte("two"), []byte("three")}}, []byte("setoption name   value three")},
+	}
 
+	for i, c := range cases {
+		usiTestHelper(t, i, c.in, c.want)
+	}
 }
 
 func TestSpin_GetName(t *testing.T) {
+	cases := []struct {
+		in   Spin
+		want []byte
+	}{
+		{Spin{[]byte("spn-nm"), 123, 0, -100, 1000}, []byte("spn-nm")},
+		{Spin{[]byte("spn-nm2"), -500, -100, -10000, 1000}, []byte("spn-nm2")},
+	}
 
+	for i, c := range cases {
+		getNameTestHelper(t, i, c.in, c.want)
+	}
 }
 
 func TestSpin_Usi(t *testing.T) {
+	cases := []struct {
+		in   Spin
+		want []byte
+	}{
+		{Spin{[]byte("spn-nm"), 123, 0, -100, 1000}, []byte("setoption name spn-nm value 123")},
+		{Spin{[]byte("spn-nm2"), -500, -100, -10000, 1000}, []byte("setoption name spn-nm2 value -500")},
+	}
 
+	for i, c := range cases {
+		usiTestHelper(t, i, c.in, c.want)
+	}
 }
 
 func TestString_GetName(t *testing.T) {
+	cases := []struct {
+		in   String
+		want []byte
+	}{
+		{String{[]byte("str-name"), []byte("engine.exe"), []byte("engine.exe")}, []byte("str-name")},
+		{String{Name: []byte("")}, []byte("")},
+		{String{[]byte(" "), []byte("engine.exe"), []byte("engine.exe")}, []byte(" ")},
+		{String{Name: []byte("%\n|t\t")}, []byte("%\n|t\t")},
+	}
 
+	for i, c := range cases {
+		getNameTestHelper(t, i, c.in, c.want)
+	}
 }
 
 func TestString_Usi(t *testing.T) {
+	cases := []struct {
+		in   String
+		want []byte
+	}{
+		{String{[]byte("str-name"), []byte("engine.exe"), []byte("engine.exe")}, []byte("setoption name str-name value engine.exe")},
+		{String{Name: []byte("")}, []byte("setoption name  value ")},
+		{String{[]byte(" "), []byte("engine.exe"), []byte("engine.exe")}, []byte("setoption name   value engine.exe")},
+		{String{Name: []byte("%\n|t\t")}, []byte("setoption name %\n|t\t value ")},
+	}
 
+	for i, c := range cases {
+		usiTestHelper(t, i, c.in, c.want)
+	}
 }
 
 func getNameTestHelper(t *testing.T, i int, o Option, want []byte) {
