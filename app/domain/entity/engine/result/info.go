b7ed109b6f31aa2aadbfdbc232a3e648e6b4063a
murosan <m.murota.dev@gmail.com>
2018-12-02 17:26:12 +0900

    mv

--- app/domain/entity/engine/result/info.go
+++ app/domain/entity/engine/result/info.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package info
+package result
 
 import "github.com/murosan/shogi-proxy-server/app/domain/entity/shogi"
 
