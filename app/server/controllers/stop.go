dbd79ede3b26e129f67a43337131f08e6294e799
murosan <m.murota.dev@gmail.com>
2019-06-09 10:07:54 +0900

    :sparkles: stop

--- app/server/controllers/stop.go
+++ app/server/controllers/stop.go
@@ -2,14 +2,47 @@ package controllers
 
 import (
 	"github.com/labstack/echo"
+	"go.uber.org/zap"
 	"net/http"
 
+	"github.com/murosan/shogi-board-server/app/domain/entity/engine"
+	"github.com/murosan/shogi-board-server/app/domain/model/usi"
 	"github.com/murosan/shogi-board-server/app/server/context"
 )
 
 // Stop lets the shogi engine stop thinking.
 func Stop(sbc *context.Context) func(echo.Context) error {
 	return func(c echo.Context) error {
-		return c.JSON(http.StatusOK, "")
+		name := c.QueryParam(ParamEngineName)
+		egn, ok := sbc.Engines[name]
+
+		// engine was not found
+		if !ok {
+			return c.NoContent(http.StatusNotFound)
+		}
+
+		sbc.Logger.Info(
+			"[Stop] param check",
+			zap.String("name", name),
+			zap.String("egn.Name", egn.Name),
+		)
+
+		// engine is not thinking. nothing to do
+		if egn.State == engine.NotConnected {
+			sbc.Logger.Info("[Stop]", zap.Any("nothing to do", egn.State))
+			return c.NoContent(http.StatusOK)
+		}
+
+		// execute 'quit'
+		if err := egn.Cmd.Write(usi.Quit); err != nil {
+			sbc.Logger.Error("[Stop] error", zap.Error(err))
+			return c.NoContent(http.StatusInternalServerError)
+		}
+
+		// set state and reset thought result.
+		egn.State = engine.StandBy
+		egn.FlushResult()
+
+		return c.NoContent(http.StatusOK)
 	}
 }
