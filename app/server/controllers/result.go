44067ac9d9a5e8b305b1a6477c692d6f84c34332
murosan <m.murota.dev@gmail.com>
2019-06-09 11:12:50 +0900

    :sparkles: get result

--- app/server/controllers/result.go
+++ app/server/controllers/result.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/labstack/echo"
+	"go.uber.org/zap"
 	"net/http"
 
 	"github.com/murosan/shogi-board-server/app/server/context"
@@ -10,6 +11,22 @@ import (
 // GetResult returns thought result of the shogi engine.
 func GetResult(sbc *context.Context) func(echo.Context) error {
 	return func(c echo.Context) error {
-		return c.JSON(http.StatusOK, "")
+		name := c.QueryParam(ParamEngineName)
+		egn, ok := sbc.Engines[name]
+
+		// engine was not found
+		if !ok {
+			return c.NoContent(http.StatusNotFound)
+		}
+
+		sbc.Logger.Info(
+			"[GetResult] param check",
+			zap.String("name", name),
+			zap.String("egn.Name", egn.Name),
+		)
+
+		sbc.Logger.Info("[GetResult]", zap.Any("result", egn.Result))
+
+		return c.JSON(http.StatusOK, egn.Result)
 	}
 }
